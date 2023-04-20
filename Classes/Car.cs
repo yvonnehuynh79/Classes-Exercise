49148c143e6a0bbb3381663f4b352fdcb12f27fc
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-04-20 08:52:42 -0700

    first commit

---
+++ Classes/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+		public Car()
+		{
+		}
+		public Car(string make, string model, int year)
+		{
+			make = make;
+			model = model;
+			year = year;
+
+		}
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+	}
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Sienna";
+            myCar.Year = 2018;
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
+
+
+
+            Car tesla = new Car()
+            {
+                Make = "Tesla",
+                Model = "Model 3",
+                Year = 2019
+            };
+            Console.WriteLine(tesla.Make);
+            Console.WriteLine(tesla.Model);
+            Console.WriteLine(tesla.Year);
+
+
+
+            
+            var Audi = new Car()
+            {
+                Make = "Audi",
+                Model = "Q7",
+                Year = 2022
+            };
+            Console.WriteLine(Audi.Make);
+            Console.WriteLine(Audi.Model);
+            Console.WriteLine(Audi.Year);
+
         }
+
+        
     }
 }
